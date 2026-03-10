a8255e59f0bad35282de875a88a64c086b36ec68
haifaamalinawahdan <haifanew3@gmail.com>
2026-03-10 07:11:15 +0700

    fungsi status BMI

--- ConditionalBMI.cpp
+++ ConditionalBMI.cpp
@@ -5,3 +5,13 @@ float NilaiBMI(float a, float b)
 {
     return a/(b * b);
 }
+
+string Status_BMI(float r)
+{
+    if (r >= 30)
+       return "Berat Badan Obesitas";
+    else if (r >= 25)
+       return "Berat Badan Kelebihan";
+    else
+       return "Berat Badan Normal";
+}
